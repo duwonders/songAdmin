21de3bd47620f784ded7f3e5898cb57cd1730243
AInotamm <rondonay@gmail.com>
2016-07-16 14:16:50 +0800

    asd

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -2,6 +2,7 @@ var webpack = require('webpack')
 var merge = require('webpack-merge')
 var utils = require('./utils')
 var baseWebpackConfig = require('./webpack.base.conf')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 // add hot-reload related code to entry chunks
@@ -11,13 +12,18 @@ Object.keys(baseWebpackConfig.entry).forEach(function (name) {
 
 module.exports = merge(baseWebpackConfig, {
   module: {
-    loaders: utils.styleLoaders()
+    loaders: utils.styleLoaders({extract: true})
   },
   // eval-source-map is faster for development
   devtool: '#eval-source-map',
+  vue: {
+    loaders: utils.cssLoaders({extract: true})
+  },
   plugins: [
     // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
     new webpack.optimize.OccurenceOrderPlugin(),
+    // extract css into its own file
+    new ExtractTextPlugin(utils.assetsPath('[name].css')),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin(),
     // https://github.com/ampedandwired/html-webpack-plugin
